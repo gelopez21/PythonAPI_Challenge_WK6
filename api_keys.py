43a8fa48e10a4c4ca799f9e37accd3f0cce4ac7d
gelopez21 <gelopez1984@gmail.com>
2020-11-28 16:19:13 -0600

    Updated API_KEYS

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
-# OpenWeatherMap API Key
+#OpenWeatherMap API Key
 weather_api_key = "89287ddf48309e7a296307ec12a699bf"
 
-# Google API Key
+#Google API Key
 g_key = "AIzaSyAVT3U0PA2edov0qiLeXT515-qYcvCe-w4"
